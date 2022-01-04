b66d50ffa062bfa3675ad532d0e73a6b76ba9f1f
Zephyr Mann <zephmann@gmail.com>
2022-01-04 15:17:12 -0500

    Add python solution for day 8, exercise 2.

--- day-08/python/solution2.py
+++ day-08/python/solution2.py
@@ -2,13 +2,85 @@
 import sys
 
 
-def calculate(values):
-    pass
+def sum_signals(signal_lines: list[str]) -> int:
+    total = 0
+
+    # iterate through lines of segment codes
+    for signal_line in signal_lines:
+        input_codes, output_codes = signal_line.split("|")
+
+        digit_map = _find_digits(input_codes.split())
+
+        output_value = 0
+        for segments in output_codes.split():
+            output_value *= 10
+            output_value += digit_map["".join(sorted(segments))]
+
+        total += output_value
+
+    return total
+
+
+def _find_digits(segment_codes: list[str]) -> dict:
+    """Determine which segment code corresponds to which digit. Return a
+    map of each segment code pointing to the corresponding digit.
+
+    :param segment_codes: List of string segment codes.
+    :return: Map of sorted segment codes to the corresponding digit.
+    """
+    digit_map = {}
+
+    # find digits with unique segment lengths
+    unique_lengths = {2: 1, 4: 4, 3: 7, 7: 8}
+    for segments in segment_codes:
+        digit = unique_lengths.get(len(segments))
+        if digit:
+            digit_map[digit] = set(segments)
+
+    # find digits with length 6
+    # TODO could probably combine these two loops
+    for segments in segment_codes:
+        if len(segments) != 6:
+            continue
+
+        # 9 will overlap 4 completely
+        # 0 will overlap 1 but not 4
+        # 6 doesn't overlap 4 or 1
+        if not digit_map[4].difference(segments):
+            digit_map[9] = segments
+        elif not digit_map[1].difference(segments):
+            digit_map[0] = segments
+        else:
+            digit_map[6] = set(segments)
+
+    # find digits with length 5
+    for segments in segment_codes:
+        if len(segments) != 5:
+            continue
+
+        # 3 will overlap 1 completely
+        # 5 will overlaps all but 1 of 6
+        # 2 doesn't overlap 1 and overlaps all but 2 of 6
+        if not digit_map[1].difference(segments):
+            digit_map[3] = segments
+        elif len(digit_map[6].difference(segments)) == 1:
+            digit_map[5] = segments
+        else:
+            digit_map[2] = segments
+
+    # can't hash a set.
+    return {
+        "".join(sorted(segments)): digit
+        for digit, segments in digit_map.items()
+    }
 
 
 def main():
     with open(sys.argv[1]) as f:
-        depth_readings = f.readlines()
+        signal_lines = f.readlines()
+
+    num_unique_digits = sum_signals(signal_lines)
+    print(f"Sum of output values is {num_unique_digits}")
 
 
 if __name__ == "__main__":
--- day-08/python/test_solution2.py
+++ day-08/python/test_solution2.py
@@ -7,9 +7,38 @@ import solution2
 @pytest.mark.parametrize(
     "values, expected",
     [
-        ([], 0),
+        (
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab",
+            {
+                "abcdefg": 8,
+                "bcdef": 5,
+                "acdfg": 2,
+                "abcdf": 3,
+                "abd": 7,
+                "abcdef": 9,
+                "bcdefg": 6,
+                "abef": 4,
+                "abcdeg": 0,
+                "ab": 1,
+            }
+        ),
+        (
+            "gd deg cdag cfade gaebf fdaeg gdcaef ebacdf cbdefg gfdabec",
+            {
+                "dg": 1,
+                "deg": 7,
+                "acdg": 4,
+                "acdef": 5,
+                "abefg": 2,
+                "adefg": 3,
+                "acdefg": 9,
+                "abcdef": 6,
+                "bcdefg": 0,
+                "abcdefg": 8,
+            }
+        ),
     ]
 )
-def test_solution(values, expected):
-    result = solution2.calculate(values)
+def test_find_digits(values, expected):
+    result = solution2._find_digits(values.split())
     assert result == expected
